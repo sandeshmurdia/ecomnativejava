e2504ce15a49e7f7a0e10e89beffb7ca511e3200
sandeshmurdia <sandeshmurdia123@gmail.com>
2025-03-20 00:15:59 +0530

    changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,12 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+
+    repositories {
+        flatDir {
+            dirs 'libs'
+        }
+    }
 }
 
 dependencies {
@@ -51,6 +57,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation files("libs/zipyandroid-release.aar")
 
     // Navigation Component
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
@@ -77,4 +84,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation(files("libs/zipyandroid-release.aar"))
 } 
--- app/src/main/java/com/example/ecommerceapp/EcommerceApplication.kt
+++ app/src/main/java/com/example/ecommerceapp/EcommerceApplication.kt
@@ -1,7 +1,17 @@
 package com.example.ecommerceapp
 
 import android.app.Application
+import com.zipy.zipyandroid.Zipy
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class EcommerceApplication : Application() 
+class EcommerceApplication : Application() {
+    
+    override fun onCreate() {
+        super.onCreate()
+        
+        // Initialize Zipy SDK
+        Zipy.setApplicationContext(this)
+        Zipy.init("6d2b9556")
+    }
+} 
